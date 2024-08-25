29884bb029f73dfbdb5e3c06466ea3920f5b94e9
kagan <kagancancubukcu@gmail.com>
2024-08-25 20:24:52 +0300

    Added ProductDetail and Store component features

---
+++ frontend/src/app/pages/Store.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../components/shared/ProductCard';
+import { Product } from '../types/Product.interface';
+
+const Store: React.FC = () => {
+  const [data, setData] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:27017/api/products')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => setData(data))
+      .catch((error) => {
+        console.error('Veri çekerken bir hata oluştu:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Store</h1>
+      <div className="flex flex-wrap gap-4 justify-center mb-10">
+        {data.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Store;
